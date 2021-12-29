845905ef18dfdf56e58944ad4ed3f3a9c6657fa9
liuyi <liuyi@gennlife.com>
2021-12-29 18:40:29 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,17 +8,17 @@ export default defineConfig(({ mode }) => {
     plugins: [vue()],
     resolve: {
       extensions: ['.ts', 'js', '.vue', '.styl', '.json'],
-        alias: {
-          '@': resolve('src'),
-          '@api': resolve('api'),
-          '@img': resolve('assets/img'),
-          '@config': resolve('config'),
-          '@mixins': resolve('mixins'),
-          '@router': resolve('router'),
-          '@store': resolve('store'),
-          '@utils': resolve('utils'),
-        },
+      alias: {
+        '@': resolve('src'),
+        '@api': resolve('api'),
+        '@img': resolve('assets/img'),
+        '@config': resolve('config'),
+        '@mixins': resolve('mixins'),
+        '@router': resolve('router'),
+        '@store': resolve('store'),
+        '@utils': resolve('utils'),
       },
+    },
     server: {
       host: '0.0.0.0',
       port: 9000,
@@ -31,6 +31,16 @@ export default defineConfig(({ mode }) => {
           rewrite: (path) => path.replace(/^\/xxxx/, ''),
         },
       },
+    },
+    css: {
+      preprocessorOptions: {
+        stylus: {
+          modifyVars: {
+            hack: `true; @import (reference) "${resolve('src/assets/styles/variables.styl')}";`,
+          },
+          javascriptEnabled: true
+        }
+      }
     },
   }
 })
