f4a04dc2e39d9e5072c5dea72465f4d2a185fe39
liuyi <13403111884@163.com>
2022-01-01 20:51:53 +0800

    -

--- src/layout/ref.ts
+++ src/layout/ref.ts
@@ -1,17 +1,23 @@
-import { ref, reactive } from 'vue'
+import { ref, reactive, h } from 'vue'
 
 export const collapsed = ref(false)
 export const activeKey = ref(null)
+
+function renderIcon (icon: string) {
+  return () => h('i', { class: icon })
+}
 
 export const menuOptions = reactive([
   {
     label: '且听风吟',
     key: 'hear-the-wind-sing',
+    icon: renderIcon('iconfont gennlife_Right'),
   },
   {
     label: '1973年的弹珠玩具',
     key: 'pinball-1973',
     disabled: true,
+    icon: renderIcon('iconfont gennlife_left'),
     children: [
       {
         label: '鼠',
@@ -23,10 +29,12 @@ export const menuOptions = reactive([
     label: '寻羊冒险记',
     key: 'a-wild-sheep-chase',
     disabled: true,
+    icon: renderIcon('iconfont gennlife_Layer'),
   },
   {
     label: '舞，舞，舞',
     key: 'dance-dance-dance',
+    icon: renderIcon('iconfont gennlife_Show_File'),
     children: [
       {
         type: 'group',
